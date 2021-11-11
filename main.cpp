75c84aa4ffa8f1758f35f29179f92c2c49a65114
Daniil <daniil_tevs@mail.ru>
2021-11-11 11:40:56 +0200

    Third

--- main.cpp
+++ main.cpp
@@ -12,18 +12,21 @@ int main()
     //Первый прямоугольник
     sf::RectangleShape shape1(sf::Vector2f(120.f, 50.f));
     shape1.setFillColor(sf::Color::Red);
-    shape1.setOrigin(60,25);
+    float shape1_x0=60,shape1_y0=25;
+    shape1.setOrigin(shape1_x0,shape1_y0);
     float shape1_x=1000, shape1_y=60,s1=5;
     //Второй прямоугольник
     sf::RectangleShape shape2(sf::Vector2f(240.f, 100.f));
     shape2.setFillColor(sf::Color::Yellow);
-    shape2.setOrigin(120,50);
+    float shape2_x0=120,shape2_y0=50;
+    shape2.setOrigin(shape2_x0,shape2_y0);
     float shape2_x=650, shape2_y=180,s2=3;
     //Третий прямоугольник
     sf::RectangleShape shape3(sf::Vector2f(70.f, 400.f));
     shape3.setFillColor(sf::Color::Green);
-    shape3.setOrigin(35,300);
-    float shape3_x=1000, shape3_y=600,s3=7;
+    float shape3_x0=35,shape3_y0=200;
+    shape3.setOrigin(shape3_x0,shape3_y0);
+    float shape3_x=1000, shape3_y=500,s3=7;
     while (window.isOpen())
     {
         sf::Event event{};
@@ -32,17 +35,17 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if(shape1_x-s1 >= 60)
+        if(shape1_x-s1 >= shape1_x0)
             shape1_x -= s1;
-        else if(shape1_x != 60)
+        else if(shape1_x != shape1_x0)
             shape1_x--;
-        if(shape2_x-s2 >= 120)
+        if(shape2_x-s2 >= shape2_x0)
             shape2_x -= s2;
-        else if(shape2_x != 120)
+        else if(shape2_x != shape2_x0)
             shape2_x--;
-        if(shape3_x-s3 >= 35)
+        if(shape3_x-s3 >= shape3_x0)
             shape3_x -= s3;
-        else if(shape3_x != 35)
+        else if(shape3_x != shape3_x0)
             shape3_x--;
         shape1.setPosition(shape1_x,shape1_y);
         shape2.setPosition(shape2_x,shape2_y);
